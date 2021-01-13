3d150d40e3e08402cc9e92eb0b23851cf253d730
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-01-14 00:49:57 +0300

    Исправляет настройки Lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,9 +5,9 @@ module.exports = {
     },
     'assert': {
       'assertions': {
-        'categories:performance': ['warn', {'minScore': 0.9}],
+        'categories:performance': ['error', {'minScore': 0.9}],
         'categories:accessibility': ['error', {'minScore': 1}],
-        'categories:best-practices': ['warn', {'minScore': 0.95}],
+        'categories:best-practices': ['error', {'minScore': 0.95}],
         'categories:seo': ['error', {'minScore': 1}]
       }
     },
