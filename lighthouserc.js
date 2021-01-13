6c7a77040ab83f36a55a6889dd240d744bf183c0
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-01-14 00:46:50 +0300

    Исправляет настройки Lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,14 @@
 module.exports = {
   'ci': {
     'collect': {
-      'staticDistDir': 'dist',
+      'staticDistDir': './dist',
     },
     'assert': {
       'assertions': {
         'categories:performance': ['warn', {'minScore': 0.9}],
-        'categories:accessibility': ['error', {'minScore': 1}]
+        'categories:accessibility': ['error', {'minScore': 1}],
+        'categories:best-practices': ['warn', {'minScore': 0.95}],
+        'categories:seo': ['error', {'minScore': 1}]
       }
     },
     'upload': {
