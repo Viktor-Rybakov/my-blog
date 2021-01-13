97031e0f246a81d8f8702b3f3ae61edc89481dbe
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-01-14 00:33:14 +0300

    Добавляет настройки Lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,16 @@
 module.exports = {
-  ci: {
-    collect: {
-      staticDistDir: 'dist',
+  'ci': {
+    'collect': {
+      'staticDistDir': 'dist',
     },
-    upload: {
-      target: 'temporary-public-storage',
+    'assert': {
+      'assertions': {
+        'categories:performance': ['warn', {'minScore': 0.9}],
+        'categories:accessibility': ['error', {'minScore': 1}]
+      }
+    },
+    'upload': {
+      'target': 'temporary-public-storage',
     },
   },
 };
